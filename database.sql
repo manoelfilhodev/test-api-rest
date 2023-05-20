0e4e8ab088487a144db4bc7a0858ec2d69ff34d9
Manoel Filho <101355923+manoelfilhodev@users.noreply.github.com>
2023-05-19 23:23:19 -0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,19 +1,44 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+-- Server version	5.7.23-23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clubes`
+--
+
 DROP TABLE IF EXISTS `clubes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clubes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `clube` varchar(100) NOT NULL,
-  `saldo_disponivel` varchar(100) DEFAULT NULL,
+  `saldo_disponivel` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `clubes`
+--
 
 LOCK TABLES `clubes` WRITE;
 /*!40000 ALTER TABLE `clubes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clubes` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `recursos`
+--
 
 DROP TABLE IF EXISTS `recursos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -21,11 +46,14 @@ DROP TABLE IF EXISTS `recursos`;
 CREATE TABLE `recursos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `recurso` varchar(256) NOT NULL,
-  `saldo_disponivel` varchar(50) DEFAULT NULL,
+  `saldo_disponivel` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `recursos`
+--
 
 LOCK TABLES `recursos` WRITE;
 /*!40000 ALTER TABLE `recursos` DISABLE KEYS */;
@@ -41,3 +69,5 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-19 23:21:57
